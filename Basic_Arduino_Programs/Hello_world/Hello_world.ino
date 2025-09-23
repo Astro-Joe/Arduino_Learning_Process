ad6f8b65191d5a5d818f2c84915bbdd759bb4dc0
root <root@localhost.localdomain>
2025-09-23 21:41:26 +0000

    Modified and compiled the ino file

--- Basic_Arduino_Programs/Hello_world/Hello_world.ino
+++ Basic_Arduino_Programs/Hello_world/Hello_world.ino
@@ -1,17 +1,20 @@
 // Initializing arduino pins for the components 
 int led = 13;
-int button = 2;
+//int button = 2;
 
 void setup() {
   // Initializing pin modes for the components 
   pinMode(led, OUTPUT);
-  pinMode(button, INPUT);
+  //pinMode(button, INPUT);
 }
 
 void loop() {
   // Sets led on when button is pressed(state is high assuming pull down is used)
-  if(digitalRead(button) == HIGH){
-    digitalWrite(led, HIGH);
-  }
+  //if(digitalRead(button) == HIGH){
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(500);
+} 
 
-}
+

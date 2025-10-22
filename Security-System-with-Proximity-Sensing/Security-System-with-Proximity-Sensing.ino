934d09884f218c12a2adab21d5094a301459d0d2
Astro-Joe <ilemobayojoseph06@gmail.com>
2025-10-22 12:31:15 +0100

    Sign Up and Login display || Refactored loading_animation function || Commented out lines, only for debugging purpose

--- Security-System-with-Proximity-Sensing/Security-System-with-Proximity-Sensing.ino
+++ Security-System-with-Proximity-Sensing/Security-System-with-Proximity-Sensing.ino
@@ -1,8 +1,6 @@
 #include <SD.h>
 #include <SPI.h>
-#include <Wire.h>
 #include <RTClib.h>
-#include <Key.h>
 #include <Keypad.h>
 #include <LiquidCrystal.h>
 #include <HCSR04.h>
@@ -51,18 +49,18 @@ void display(String sentence) {
 }
 
 //---Loading Animation---
-void loading_animation(unsigned char char_length){
+void loading_animation(unsigned char col, unsigned char row){
   for (unsigned char i = 0; i < 3; i++) {
-    lcd.setCursor(char_length , 0); // cycles between 3 dots
+    lcd.setCursor(col, row); // cycles between 3 dots
     lcd.print(".");
     delay(300);
-    lcd.setCursor(char_length, 0);
+    lcd.setCursor(col, row);
     lcd.print("..");
     delay(300);
-    lcd.setCursor(char_length, 0); // cycles between 3 dots
+    lcd.setCursor(col, row); // cycles between 3 dots
     lcd.print("...");
     delay(300);
-    lcd.setCursor(char_length, 0); // cycles between 3 dots
+    lcd.setCursor(col, row); // cycles between 3 dots
     lcd.print("   ");
   }
   lcd.noBlink();
@@ -86,12 +84,12 @@ void setup() {
   lcd.clear();
 
   display("System Init");
-  loading_animation(11);
+  loading_animation(11, 0);
   lcd.clear();
   
 
   display("RTC Init");
-  loading_animation(8);
+  loading_animation(8, 0);
   lcd.clear();
 
   if (!rtc.begin()) {
@@ -109,7 +107,7 @@ void setup() {
   pinMode(CS_Pin, OUTPUT);
   digitalWrite(CS_Pin, HIGH);
   display("SD Init");
-  loading_animation(7);
+  loading_animation(7, 0);
   lcd.clear();
 
   if (!SD.begin(CS_Pin)) {
@@ -122,46 +120,62 @@ void setup() {
   lcd.clear();
   }
 
+  //---Password entry display---
+  lcd.setCursor(0, 0);
+  lcd.print("Press a number");
+  lcd.setCursor(0, 1);
+  lcd.print("to select");
+  delay(3000);
+
+  lcd.clear();                                                                                                                                                                                                                               
+
+  lcd.print("1. New Password");
+  delay(1000);
+  lcd.setCursor(0, 1);
+  lcd.print("2. Login");
+
 } 
 
 void loop() { 
   distance = hc.dist();
 
   unsigned long now = millis();
-  Serial.print("millis: ");
-  Serial.print(now);
-  Serial.print("   ");
+
+  /*Serial.print("millis: "); //Debugging purpose 
+  Serial.print(now); 
+  Serial.print("   "); 
 
   Serial.print("distance: ");
   Serial.print(distance);
-  Serial.println();
+  Serial.println(); */
 
-  lcd.setCursor(0, 0);
+  /*lcd.setCursor(0, 0);
   lcd.print("DistKeypad:");
   lcd.print(distance);
-  lcd.print("cm   ");
+  lcd.print("cm   "); //Debugging purpose */
 
-  unsigned long current_millis = millis();
-  if (current_millis < 18497UL) {
+  if (now < 23512UL) {
     digitalWrite(lcd_backlight, BACKLIGHT_ON_LEVEL);
   }
   else {
     // after 6000 ms -> follow the distance rule
-    if (distance <= 29.0 && distance > 6.0) {
+    if (distance <= 30.0 && distance > 6.0) {
       digitalWrite(lcd_backlight, BACKLIGHT_ON_LEVEL);   // ON
-      lcd.setCursor(0, 1);
-      lcd.print("LCDbacklight ON!");
+      /*lcd.setCursor(0, 1);
+      lcd.print("LCDbacklight ON!"); */ //Debugging purpose
     } 
     else if (distance < 5.0) {
       digitalWrite(lcd_backlight, BACKLIGHT_OFF_LEVEL);  // OFF
-      lcd.setCursor(0, 1);
-      lcd.print("                  ");
+      /*lcd.setCursor(0, 1);
+      lcd.print("                  "); */ //Debugging purpose
     }
     else {
       digitalWrite(lcd_backlight, BACKLIGHT_OFF_LEVEL);
-      lcd.setCursor(0, 1);
-      lcd.print("                  ");
+      /*lcd.setCursor(0, 1);
+      lcd.print("                  "); */ //Debugging purpose
     }
   }
+
+  
   delay(300);
 }
